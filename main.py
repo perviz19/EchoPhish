819465fe146385bc84bd29eeba601e92c32a3fc2
perviz_muslumov <157914250+perviz19@users.noreply.github.com>
2024-06-24 04:39:29 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import time
 import json
 import signal
 import sys
+import os
 from colorama import Fore, Style
+from app.functions import banner1
 
 def cleanup():
     subprocess.run(["pkill", "-f", "python3 web_app.py"], check=True)
@@ -16,25 +18,51 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 try:
-    subprocess.Popen(["python3", "web_app.py"])
+    os.system("clear")
+    banner1()
+    message = f"""\n{Style.BRIGHT}{Fore.YELLOW}
+    Select the method!
+-------------------  
+1-{Fore.GREEN} localhost{Fore.YELLOW}       |
+-------------------
+2-{Fore.GREEN} cloudflared{Fore.YELLOW}     |
+-------------------"""
     
-    time.sleep(2)
 
-    subprocess.Popen(["cloudflared", "tunnel", "--no-autoupdate", "--metrics", "localhost:55555", "--url", "http://localhost:8080"], stderr=subprocess.DEVNULL)
-    print("Cloudflared starintg....")
-
-    time.sleep(4)
-
-    result = subprocess.run(["curl", "-s", "http://localhost:55555/quicktunnel"], capture_output=True, text=True)
-    time.sleep(1)
-    if result.stdout.strip():
-        data = json.loads(result.stdout.strip())
-        url = f"http://{data['hostname']}"
-        message = f"{Style.BRIGHT}{Fore.GREEN}------ URL: \n{url}{Style.RESET_ALL}"
+    print(message)
+    choose = input()
+    if choose == '2':
+        subprocess.Popen(["python3", "web_app.py"])
+        time.sleep(2)
+        os.system("clear")
+        banner1()
+        subprocess.Popen(["cloudflared", "tunnel", "--no-autoupdate", "--metrics", "localhost:55555", "--url", "http://localhost:8080"], stderr=subprocess.DEVNULL)
+        print(f"{Fore.YELLOW}Cloudflared starting....")
+        time.sleep(4)
+        result = subprocess.run(["curl", "-s", "http://localhost:55555/quicktunnel"], capture_output=True, text=True)
+        time.sleep(1)
+        if result.stdout.strip():
+            data = json.loads(result.stdout.strip())
+            url = f"https://{data['hostname']}"
+            message = f"""{Style.BRIGHT}{Fore.YELLOW} 
+            URLs: 
+------------------------------------------------
+{Fore.GREEN}{url}{Fore.YELLOW}
+------------------------------------------------
+"""
+            print(message)
+        else:
+            raise ValueError("Empty response or invalid JSON format")
+    elif choose == '1':
+        subprocess.Popen(["python3", "web_app.py"])
+        time.sleep(2)
+        os.system("clear")
+        banner1()
+        message = f"{Style.BRIGHT}{Fore.GREEN}-URL: \nhttp://127.0.0.1:8080{Style.RESET_ALL}"
         print(message)
+    
     else:
-        raise ValueError("Empty response or invalid JSON format")
-
+        print(f"{Fore.RED}Please enter a valid number!")
     signal.pause()
 
 except json.JSONDecodeError as e:
